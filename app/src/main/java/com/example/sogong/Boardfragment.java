c4665fd6b66e09c80a11817d30a30392249de480
Ghanadara <rnjs1522@naver.com>
2022-08-16 00:29:11 +0900

    20220816 메인화면 수정

--- app/src/main/java/com/example/sogong/Boardfragment.java
+++ app/src/main/java/com/example/sogong/Boardfragment.java
@@ -18,7 +18,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-
+//ㅁㅁ
 public class Boardfragment extends Fragment {
     private static final String TAG = "Boardfragment";
     private RecyclerView postrecyclerView;
